e6270e03482417aa91853cf0f7803f9e2333055e
Jay Anthon Rico <jayanthonrico23@gmail.com>
2023-06-03 19:23:51 +0800

    added responsiveness to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,13 @@ import logo from "../assets/logo.svg";
 
 export const Navbar = () => {
   return (
-    <nav className="text-neutral-white font-raleway capitalize flex items-center justify-between px-7 py-9">
-      <img src={logo} alt="fylo-logo" className="w-24 tablet:w-[20vh]" />
-      <ul className="flex gap-5 text-xs ">
+    <nav className="text-neutral-white font-raleway capitalize flex items-center justify-between px-7 py-9 laptop:px-24 tablet:px-16">
+      <img
+        src={logo}
+        alt="fylo-logo"
+        className="w-24 tablet:w-36 laptop:w-40 desktop:w-44"
+      />
+      <ul className="flex gap-5 text-xs tablet:text-lg laptop:text-base laptop:gap-16 ">
         {routes.map((route) => {
           return (
             <li key={route.path} className="cursor-pointer">
